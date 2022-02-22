cf07b414955807dea744017938683f073b43218e
priyanka <priyanka.chavan1408@gmail.com>
2022-02-22 17:07:00 +0530

    ADDED:Welcome message on the master branch.

---
+++ DataStructure.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.datastructure;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        System.out.println("Welcome message on the master branch");
+    }
+}
+
